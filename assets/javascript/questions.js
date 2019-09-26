345fc084fa961dabe230281a5cb36987adc56eae
Katherine Ray <katherine.ray@me.com>
2019-09-26 13:47:51 -0500

    added style to html page and began variables array for trivia question file

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,1 +1,19 @@
-console.log("Question;");
+//Create variable triviaQuestions
+//Create array to list all triviaQuestions
+const triviaQuestions = [
+  {
+    Question: "",
+    Choices: [1, 2, 3, 4],
+    correctAnswer: ""
+  },
+  {
+    Question: "",
+    Choices: [1, 2, 3, 4],
+    correctAnswer: ""
+  },
+  {
+    Question: "",
+    Choices: [1, 2, 3, 4],
+    correctAnswer: ""
+  }
+];
