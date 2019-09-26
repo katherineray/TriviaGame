f0819de0a175b1d086007a7b92faed5b88fb2126
Katherine Ray <katherine.ray@me.com>
2019-09-26 16:19:01 -0500

    completed triviaQuestions on questions.js & confirmed working in console.log in app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,3 @@
-console.log($);
+console.log(triviaQuestions);
+//Note: All data from questions.js file will be available in app.js due to listing on index.htlml file
+//You do not need to code in both .js files to run data
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -2,18 +2,32 @@
 //Create array to list all triviaQuestions
 const triviaQuestions = [
   {
-    Question: "",
-    Choices: [1, 2, 3, 4],
-    correctAnswer: ""
+    Question: "What year was the War of 1812?",
+    Choices: ["1812", "1920", "Yesterday"],
+    correctAnswer: "1812"
   },
   {
-    Question: "",
-    Choices: [1, 2, 3, 4],
-    correctAnswer: ""
+    Question: "Who went on the Lewis and Clark expedition?",
+    Choices: ["Andrew and Steven", "Lewis and Clark", "George and Lincoln"],
+    correctAnswer: "Lewis and Clark"
   },
   {
-    Question: "",
-    Choices: [1, 2, 3, 4],
-    correctAnswer: ""
+    Question: "Who fought the French and Indian War?",
+    Choices: [
+      "The Spanish and Brittish",
+      "The Chinese and Japanese",
+      "The French and the Indians"
+    ],
+    correctAnswer: "The French and the Indians"
+  },
+  {
+    Question: "Which war came first: World War One or World War Two?",
+    Choices: ["World War Two", "Neither", "World War One"],
+    correctAnswer: "World War One"
+  },
+  {
+    Question: "Where is South Africa?",
+    Choices: ["In the USA", "In Africa", "In China"],
+    correctAnswer: "In Africa"
   }
 ];
