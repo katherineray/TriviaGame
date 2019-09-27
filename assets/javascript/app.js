c64cc02e31ddbdc0069906bbc9ecb413ffd738ec
Katherine Ray <katherine.ray@me.com>
2019-09-26 21:23:34 -0500

    trouble shooting syntax error in app.js, console is focused on prior deleted changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 //Initial Values
 var counter = 30; //Timer that goes from 30 to 0 value
-var currentQuestion = 2; //pulls questions from array, by default pulls first question
+var currentQuestion = 0; //pulls questions from array, by default pulls first question
 var score = 0; //counts how many questions the user has selected correctly
 var lost = 0; //increment of how many questions the user has answered incorrectly
 var timer; //holds the set interval. Allows reset of time
@@ -8,12 +8,18 @@ var timer; //holds the set interval. Allows reset of time
 //Display the questions and the choices in the browser
 
 function loadQuestion() {
-  //function to call currentQuestion and run choices in document page
-  const question = triviaQuestions[currentQuestion].question; //how to run questions
-  const choices = triviaQuestions[currentQuestion].choices; //how to run choices
-
-  $("#game").html("<h4>" + question + "</h4>");
-}
+	//function to call currentQuestion and run choices in document page
+	const question = triviaQuestions[currentQuestion].question; //how to run questions
+	const choices = triviaQuestions[currentQuestion].choices; //how to run choices
+	$("#time").html("Timer: " + counter);
+	$("#game").html(`<h4>${question}</h4>`);
+    loadChoices(choices);    //calling choices array
+}	
+function loadChoices(choices) {
+	var result = "";
+	for (let i = 0; i < choices.length; i++) {  //looping p tags
+		result += `<p class='choice' data-answer="${choices[i]}">${choices[i]}</p>`; //data to be saved for all choices
+ 		}
+    return result;
 
 loadQuestion(); //calling Question
-console.log(currentQuestion); // Receiving an undefined value until the letter case was changed in the Question & Choices questions.js
